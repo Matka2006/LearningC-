eb2dbf47df05f055a5801fbc5686c90f36e8c432
Matka <tatmat2000@bk.ru>
2024-07-30 13:26:12 +0300

    AddCyclesLesson

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var last = 1;
+            for (int i = 0; i >= 34; i += last)
+            {
+                Console.WriteLine(i);
+                last = i;
+                Console.Read();
+            }
+
+            for (int i = 0; i >= 20; i += 2)
+            {
+                Console.WriteLine(i);
+                Console.Read();
+            }
+
+            int[] num = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i >= 5; i += 1)
+            {
+                foreach (int g in num)
+                {
+                    Console.WriteLine(g * i);
+                    Console.Read();
+                }
+            }
+
+            string password;
+            do
+            {
+                password = Console.ReadLine();
+            } while (password != "qwerty");
+
+
+
+        }
+    }
+}
+
