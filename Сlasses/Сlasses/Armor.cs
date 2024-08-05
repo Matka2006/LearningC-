19945cb8b5f4ffc2746d9941f0405d6aa9cff8b2
Matka <tatmat2000@bk.ru>
2024-08-05 14:05:40 +0300

    AddClasses

---
+++ Сlasses/Сlasses/Armor.cs
@@ -0,0 +1,57 @@
+﻿namespace Сlasses
+{
+    public class Helm
+    {
+        public string Name { get; }
+
+        public float Armor { get; set; }
+
+        public Helm(string name = "Helm")
+        {
+            Name = name;
+        }
+
+        public Helm(float armor, string name = "Helm")
+        {
+            Armor = armor;
+            Name = name;
+        }
+    }
+
+    public class Shell
+    {
+        public string Name { get; }
+
+        public float Armor { get; set; }
+
+        public Shell(string name = "Shell")
+        {
+            Name = name;
+        }
+
+        public Shell(float armor, string name = "Shell")
+        {
+            Name = name;
+            Armor = armor;
+        }
+    }
+
+    public class Boots
+    {
+        public string Name { get; }
+
+        public float Armor { get; set; }
+
+        public Boots(string name = "Boots")
+        {
+            Name = name;
+        }
+
+        public Boots(float armor, string name = "Boots")
+        {
+            Name = name;
+            Armor = armor;
+        }
+    }
+}
+
---
+++ Сlasses/Сlasses/Unit.cs
@@ -0,0 +1,71 @@
+﻿namespace Сlasses
+{
+    public class Unit
+    {
+        private float _health;
+        private float _armor;
+
+        private Helm _helm;
+        private Shell _shell;
+        private Boots _boots;
+        private Weapon _weapon;
+
+        Weapon Damage = new Weapon(1f, 10f, "Weapon");
+
+        public string Name { get; }
+        public float Health => _health;
+
+        public float Armor
+        {
+            get { return (float)Math.Round(_armor, 2); }
+            set
+            {
+                if (value >= 0 || value <= 1)
+                {
+                    _armor = value;
+                }
+                else
+                {
+                    Console.WriteLine("Осторожно");
+                    Console.Read();
+                }
+            }
+        }
+
+
+        public Unit() : this("Unknown Unit")
+        {
+        }
+
+        public Unit(string name, float health)
+        {
+            Name = name;
+            _health = health;
+        }
+
+        public float RealHealth()
+        {
+            return Health * (1 + Armor);
+        }
+
+        public bool SetDamage()
+        {
+            _health = Health - value * Armor;
+            if (Health <= 0f)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void EquipWeapon(Weapon weapon) { _weapon = weapon; }
+        public void EquipHelm(Helm helm) { _helm = helm; }
+        public void EquipShell(Shell shell) { _shell = shell; }
+        public void EquipBoots(Boots boots) { _boots = boots; }
+
+    }
+}
+
